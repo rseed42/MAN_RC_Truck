8a0a66300daf9231ffb8b4f1df9d5fe060512d68
Venelin Petkov <petkov.venelin@gmail.com>
2017-12-09 15:52:06 +0100

    Isolated control logic

--- main.py
+++ main.py
@@ -2,10 +2,7 @@
 import rospy
 from std_msgs.msg import UInt32
 from sensor_msgs.msg import CompressedImage
-from sensor_msgs.msg import CameraInfo
-import random
 import time
-import os
 import sys
 import pygame
 from pygame.locals import *
@@ -20,85 +17,23 @@ import attrdict
 # Configuration
 # ------------------------------------------------------------------------------
 CONFIG_FILE_NAME = 'config.yml'
-MSG_QUEUE_SIZE = 10
-TOPIC_THROTTLE = '/controller/throttle'
-TOPIC_STEERING = '/controller/steering'
-TOPIC_SHIFT = '/controller/shift'
-TOPIC_LEG = '/controller/leg'
-TOPIC_IMAGE = '/raspicam_node/image/compressed'
-
-FRAMERATE_AVG_COUNT = 15
-
-IMAGE_WIDTH = 640
-IMAGE_HEIGHT = 480
-#IMAGE_WIDTH = 800
-#IMAGE_HEIGHT = 600
-
-CLOCK_RATE = 60
-THROTTLE_DEFAULT = 1500
-STEERING_DEFAULT = 1500
-SHIFT_DEFAULT = 1500
-LEG_DEFAULT = 1500
-THROTTLE_INCREMENT = 5
-STEERING_INCREMENT = 5
-SHIFT_INCREMENT = 5
-LEG_INCREMENT = 5
-# ------------------------------------------------------------------------------
-# Init
-# ------------------------------------------------------------------------------
-def init_servo():
-    pub = rospy.Publisher(TOPIC_THROTTLE, UInt32, queue_size=MSG_QUEUE_SIZE)
-    rospy.init_node('publisher', anonymous=True)
-    pub.publish(1500)
-# ------------------------------------------------------------------------------
-# Dispatch
-# ------------------------------------------------------------------------------
-def dispatch(key):
-    global running
-    if key == u'\x1bOA':
-        print 'forward'
-    elif key == u'\x1bOB':
-        print 'backward'
-    elif key == u'\x1bOD':
-        print 'left'
-    elif key == u'\x1bOC':
-        print 'right'
-    elif key == u'q':
-        running = False
-# ------------------------------------------------------------------------------
-# Producer
-# ------------------------------------------------------------------------------
-#    try:
-#        producer()
-#    except rospy.ROSInterruptException:
-#        pass
-def producer():
-    pub = rospy.Publisher(TOPIC_THROTTLE, UInt32, queue_size=MSG_QUEUE_SIZE)
-    rospy.init_node('publisher', anonymous=True)
-    rate = rospy.Rate(0.5)
-    while not rospy.is_shutdown():
-        msg = random.randint(1000, 2000)
-        print "message: %d" % msg
-        pub.publish(msg)
-        rate.sleep()
-        rate = rospy.Rate(random.uniform(0.1, 1))
-
 # ------------------------------------------------------------------------------
 # Camera Sensor
 # ------------------------------------------------------------------------------
 class ImageSensor:
-    def __init__(self, framerate_avg_count):
+    def __init__(self, configuration):
         # Over how many frames back to calculate the moving average
-        self._framerate_avg_count = framerate_avg_count
+        self._framerate_avg_count = configuration.sensor.image.framerate_avg_count
         self.average_framerate = 0
         # Used to measure frame length duration
         self.last_time = 0
         self.image_counter = 0L
         self.bridge = CvBridge()
-        self.last_image = np.zeros((IMAGE_WIDTH, IMAGE_HEIGHT, 3), dtype=np.uint8)
+        self.last_image = np.zeros((configuration.sensor.image.width, configuration.sensor.image.height, 3), dtype=np.uint8)
+        self._sub = None
 
     def register(self, topic):
-        self._sub = rospy.Subscriber(TOPIC_IMAGE, CompressedImage, self.on_image_update)
+        self._sub = rospy.Subscriber(topic, CompressedImage, self.on_image_update)
         self.last_time = time.time()
 
     def on_image_update(self, message_data):
@@ -127,151 +62,198 @@ class ImageSensor:
         self.last_time = now
 
 # ------------------------------------------------------------------------------
+# Controller
+# ------------------------------------------------------------------------------
+class ControlChannel(object):
+    """
+    Base class for control signals. By default nothing changes. Subclass to override channel's behaviour
+    """
+    def __init__(self, default, key_increase, key_decrease):
+        # Used to filter out the signals
+        self.key_increase = key_increase
+        self.key_decrease = key_decrease
+        self.default = default
+        self.last_signal = default
+        self.signal = default
+        self._pub = None
+
+    def register_publisher(self, topic, queue_size):
+        self._pub = rospy.Publisher(topic, UInt32, queue_size=queue_size)
+
+    def _send(self):
+        self._pub.publish(self.signal)
+
+    def _send_default(self):
+        self.last_signal = self.signal
+        self.signal = self.default
+        self._send()
+
+    def on_key_increase_up(self):
+        self._send_default()
+
+    def on_key_decrease_up(self):
+        self._send_default()
+
+    def on_key_increase_down(self):
+        self._send_default()
+
+    def on_key_decrease_down(self):
+        self._send_default()
+
+    def on_key_increase_pressed(self):
+        self._send_default()
+
+    def on_key_decrease_pressed(self):
+        self._send_default()
+
+
+class ThrottleChannel(ControlChannel):
+    """
+    Using just one speed with the throttle, as slow as possible
+    """
+    def __init__(self, default, key_increase, key_decrease):
+        super(ThrottleChannel, self).__init__(default, key_increase, key_decrease)
+
+    def on_key_increase_pressed(self):
+        self.last_signal = self.signal
+        self.signal = 1400
+        self._send()
+
+    def on_key_decrease_pressed(self):
+        self.last_signal = self.signal
+        self.signal = 1600
+        self._send()
+
+
+class SteeringChannel(ControlChannel):
+    """
+    Steering needs to be fast, so we send a more powerful signal
+    """
+    def __init__(self, default, key_increase, key_decrease):
+        super(SteeringChannel, self).__init__(default, key_increase, key_decrease)
+
+    def on_key_increase_pressed(self):
+        self.last_signal = self.signal
+        self.signal = 1200
+        self._send()
+
+    def on_key_decrease_pressed(self):
+        self.last_signal = self.signal
+        self.signal = 1800
+        self._send()
+
+
+class HumanController:
+    def __init__(self, config):
+        self.cfg = config
+        #
+        self.channels = {
+            'steering': SteeringChannel(self.cfg.control.steering.default, K_LEFT, K_RIGHT),
+            'throttle': ThrottleChannel(self.cfg.control.throttle.default, K_UP, K_DOWN),
+            'shift': ControlChannel(self.cfg.control.shift.default, K_w, K_s),
+            'leg': ControlChannel(self.cfg.control.leg.default, K_a, K_d)
+        }
+
+    def register(self):
+        self.channels['steering'].register_publisher(self.cfg.topic.steering, self.cfg.control.msg_queue_size)
+        self.channels['throttle'].register_publisher(self.cfg.topic.throttle, self.cfg.control.msg_queue_size)
+        self.channels['shift'].register_publisher(self.cfg.topic.shift, self.cfg.control.msg_queue_size)
+        self.channels['leg'].register_publisher(self.cfg.topic.leg, self.cfg.control.msg_queue_size)
+
+    def on_key_up(self, event):
+        for channel in self.channels.values():
+            if event.key == channel.key_increase:
+                channel.on_key_increase_up()
+            elif event.key == channel.key_decrease:
+                channel.on_key_decrease_up()
+
+    def on_key_down(self, event):
+        for channel in self.channels.values():
+            if event.key == channel.key_increase:
+                channel.on_key_increase_down()
+            elif event.key == channel.key_decrease:
+                channel.on_key_decrease_down()
+
+    def on_key_pressed(self, keys):
+        for channel in self.channels.values():
+            if keys[channel.key_increase]:
+                channel.on_key_increase_pressed()
+            elif keys[channel.key_decrease]:
+                channel.on_key_decrease_pressed()
+
+# ------------------------------------------------------------------------------
 # App
 # ------------------------------------------------------------------------------
 class App:
-    def __init__(self, width, height, image_sensor):
+    def __init__(self, configuration, image_sensor, controller):
+        self.cfg = configuration
         self._running = True
         self._display_surf = None
-        self.width = width
-        self.height = height
-        self.size = self.width, self.height
-        self.font = pygame.font.SysFont('monospace', 15)
-        self.text_color = (255, 255, 255)
-        self.screen_color = (0, 0, 0)
+        self.font = pygame.font.SysFont(self.cfg.app.surface.font, self.cfg.app.surface.font_size)
         self.clock = pygame.time.Clock()
-        self.throttle = THROTTLE_DEFAULT
-        self.steering = STEERING_DEFAULT
-        self.shift = SHIFT_DEFAULT
-        self.leg = LEG_DEFAULT
-        self.throttle_pub = rospy.Publisher(TOPIC_THROTTLE, UInt32, queue_size=MSG_QUEUE_SIZE)
-        self.steering_pub = rospy.Publisher(TOPIC_STEERING, UInt32, queue_size=MSG_QUEUE_SIZE)
-        self.shift_pub = rospy.Publisher(TOPIC_SHIFT, UInt32, queue_size=MSG_QUEUE_SIZE)
-        self.leg_pub = rospy.Publisher(TOPIC_LEG, UInt32, queue_size=MSG_QUEUE_SIZE)
-
-
+        # Sensors
         self.image_sensor = image_sensor
-
-        # self.image_sub = rospy.Subscriber(TOPIC_IMAGE, CompressedImage, self.image_callback)
-        #
-        # self.framerate_smooth = 0
-        # self.last_time = time.time()
-        # self.image_counter = 0
-        # self.bridge = CvBridge()
-        # self.last_image = np.zeros((IMAGE_WIDTH, IMAGE_HEIGHT, 3), dtype=np.uint8)
-
-#    def image_callback(self, image):
-#        # Calculate the frame rate
-#        self.image_counter += 1
-#        now = time.time()
-#        delta = now - self.last_time
-#        framerate = 1./delta
-#        self.framerate_smooth = self.framerate_smooth + float(framerate - self.framerate_smooth)/(self.image_counter+1)
-#        self.last_time = now
-#
-#        if self.image_counter % 30 == 0:
-#            print 'Image Framerate: {}'.format(self.framerate_smooth)
-#
-#        # Get the image
-#        try:
-#            image_array = self.bridge.compressed_imgmsg_to_cv2(image, desired_encoding="passthrough")
-#            self.last_image = cv2.rotate(image_array, cv2.ROTATE_90_COUNTERCLOCKWISE)
-#        except CvBridgeError as err:
-#            print err
-#
+        self.controller = controller
 
     def on_init(self):
-        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        # Initialize the primary display surface
+        self._display_surf = pygame.display.set_mode(
+            (self.cfg.app.surface.width, self.cfg.app.surface.height),
+            pygame.HWSURFACE | pygame.DOUBLEBUF
+        )
         self._running = True
-        rospy.init_node('publisher', anonymous=True)
+        # Return true if everything went well, otherwise we have to abort the application
+        return True
 
-    def show_text(self, msg, left, right):
-        text = self.font.render(msg, True, self.text_color, (75, 75, 75))
-        self._display_surf.blit(text, (left, right))
+    def show_text(self, msg, x, y):
+        text = self.font.render(msg, True, self.cfg.text.default.color, self.cfg.text.default.background_color)
+        self._display_surf.blit(text, (x, y))
 
     def on_event(self, event):
         if event.type == pygame.KEYUP:
-            if event.key == K_UP or event.key == K_DOWN:
-                self.throttle = THROTTLE_DEFAULT
-                self.throttle_pub.publish(self.throttle)
+            self.controller.on_key_up(event)
 
-            if event.key == K_LEFT or event.key == K_RIGHT:
-                self.steering = STEERING_DEFAULT
-                self.steering_pub.publish(self.steering)
-
-            if event.key == K_w or event.key == K_s:
-                self.shift = SHIFT_DEFAULT
-                self.shift_pub.publish(self.shift)
-
-            if event.key == K_a or event.key == K_d:
-                self.leg = LEG_DEFAULT
-                self.leg_pub.publish(self.leg)
-
+        if event.type == pygame.KEYDOWN:
             if event.key == K_q:
                 self._running = False
+            self.controller.on_key_down(event)
 
         if event.type == pygame.QUIT:
             self._running = False
 
+
     def on_key_pressed(self, keys):
-        if keys[pygame.K_UP]:
-            self.throttle = max(self.throttle - THROTTLE_INCREMENT, 1000)
-            self.throttle_pub.publish(self.throttle)
-
-        if keys[pygame.K_DOWN]:
-            self.throttle = min(self.throttle + THROTTLE_INCREMENT, 2000)
-            self.throttle_pub.publish(self.throttle)
-
-        if keys[pygame.K_LEFT]:
-            self.steering = max(self.steering - STEERING_INCREMENT, 1000)
-            self.steering_pub.publish(self.steering)
-
-        if keys[pygame.K_RIGHT]:
-            self.steering = min(self.steering + STEERING_INCREMENT, 2000)
-            self.steering_pub.publish(self.steering)
-
-        if keys[pygame.K_w]:
-            self.shift = max(self.shift - SHIFT_INCREMENT, 1000)
-            self.shift_pub.publish(self.shift)
-
-        if keys[pygame.K_s]:
-            self.shift = min(self.shift + SHIFT_INCREMENT, 2000)
-            self.shift_pub.publish(self.shift)
-
-        if keys[pygame.K_a]:
-            self.leg = max(self.leg - LEG_INCREMENT, 1000)
-            self.leg_pub.publish(self.leg)
-
-        if keys[pygame.K_d]:
-            self.leg = min(self.leg + LEG_INCREMENT, 2000)
-            self.leg_pub.publish(self.leg)
+        self.controller.on_key_pressed(keys)
 
     def on_loop(self):
         pass
 
     def on_render(self):
-        self._display_surf.fill(self.screen_color)
+        self._display_surf.fill(self.cfg.app.surface.screen_color)
 
         # Render the image and all information on this frame
         surfarray.blit_array(self._display_surf, self.image_sensor.last_image)
+
         self.show_text('image fps: %.1f' % self.image_sensor.average_framerate, 0, 0)
-        self.show_text('throttle: %d' % self.throttle, 20, 20)
-        self.show_text('steering: %d' % self.steering, 20, 40)
-        self.show_text('shift: %d' % self.shift, 20, 60)
-        self.show_text('leg: %d' % self.leg, 20, 80)
 
+        # Show all control values
+        def show_signal((index, (name, channel))):
+            self.show_text('{}: {}'.format(name, channel.signal), 0, 30 + 15*index)
+        map(show_signal, enumerate(self.controller.channels.items()))
+
+        # Update the surface after we have applied all drawing operations
         pygame.display.flip()
-
-        self.clock.tick(CLOCK_RATE)
+        # Fixed fps rate for the application
+        self.clock.tick(self.cfg.app.clock_rate)
 
     def on_cleanup(self):
         pygame.quit()
 
     def on_execute(self):
-        if self.on_init() == False:
-          self._running = False
-
-        while(self._running):
+        # If we can not initialize the application, quit
+        if not self.on_init():
+            self._running = False
+        while self._running:
             self.on_key_pressed(pygame.key.get_pressed())
             for event in pygame.event.get():
                 self.on_event(event)
@@ -294,15 +276,19 @@ if __name__ == '__main__':
         sys.stderr.write('Could not find the configuration file "{}"\n'.format(CONFIG_FILE_NAME))
         sys.exit(1)
 
-    print configuration.app.name
-    sys.exit(0)
-
     pygame.init()
 
     # Create and register sensors. They start receiving messages as soon as they are registered
-    image_sensor = ImageSensor(FRAMERATE_AVG_COUNT)
-    image_sensor.register(TOPIC_IMAGE)
+    image_sensor = ImageSensor(configuration)
+    image_sensor.register(configuration.topic.camera.compressed)
 
+    # Create and register the controller
+    controller = HumanController(configuration)
+    controller.register()
 
-    app = App(IMAGE_WIDTH, IMAGE_HEIGHT, image_sensor)
+    # Initialize this application as an ROS node, otherwise we can not communicate with the ROS system
+    rospy.init_node(configuration.ros.node.name, anonymous=True)
+
+    # Instantiate the pygame app
+    app = App(configuration, image_sensor, controller)
     app.on_execute()
